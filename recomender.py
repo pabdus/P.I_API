f2836f82e042220ea0bc775360b74924354eacb7
pabdus <112780608+pabdus@users.noreply.github.com>
2023-06-08 15:21:14 -0500

    Update recomender.py

--- recomender.py
+++ recomender.py
@@ -3,21 +3,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Cargar el dataset
-df = pd.read_csv('Data_ML.csv')
+df = pd.read_csv('data_limpia.csv')
 
 # Seleccionar las columnas relevantes
-df = df[['original_title', 'overview', 'genres_name', 'popularity']]
+df = df.nlargest(5500, 'popularity')
+df = df[['title', 'overview', 'genres']]
 df = df.dropna()
-df = df.sort_values('popularity', ascending=False).iloc[:5500]
 df.reset_index(inplace=True, drop=True)
 
 # Limpiar el texto y los géneros
-
+df['overview'] = df['overview'].fillna('')
 df['overview'] = df['overview'].apply(lambda x: x.lower())
-df['genres_name'] = df['genres_name'].apply(lambda x: x.lower())
+df['genres'] = df['genres'].fillna('')
+df['genres'] = df['genres'].apply(lambda x: x.lower())
 
 # Combinar las columnas de texto y géneros en una sola columna
-df['combined_features'] = df['overview'] + ' ' + df['genres_name']
+df['combined_features'] = df['overview'] 
 
 # Vectorizar el texto combinado
 vectorizer = TfidfVectorizer(stop_words='english')
@@ -30,7 +31,7 @@ similarity_matrix = cosine_similarity(feature_matrix)
 def get_similar_movies(movie_title, n=5):
     # Buscar la película en el dataset
     try:
-        movie_index = df[df['original_title'].str.lower() == movie_title.lower()].index[0]
+        movie_index = df[df['title'].str.lower() == movie_title.lower()].index[0]
     except IndexError:
         return f"Lo siento, no se encuentra la película '{movie_title}' en el sistema de recomendaciones."
     
@@ -40,5 +41,5 @@ def get_similar_movies(movie_title, n=5):
     
     # Obtener los títulos de las películas más similares
     similar_movies = similar_movies[1:n+1]
-    similar_movies = [df.iloc[movie[0]]['original_title'] for movie in similar_movies]
+    similar_movies = [df.iloc[movie[0]]['title'] for movie in similar_movies]
     return similar_movies
